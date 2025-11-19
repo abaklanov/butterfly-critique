02a7e57f41daa3d1fcddf56ec8babc8ea4e72d03
abaklanov <alexandr.baklanov@gmail.com>
2025-11-19 15:23:39 +0200

    Add more CRUD for users

--- src/users/api.ts
+++ src/users/api.ts
@@ -1,4 +1,5 @@
 import * as Fastify from "fastify";
+import { nanoid } from "nanoid";
 
 const usersApi: Fastify.FastifyPluginCallback = function (
   fastify,
@@ -7,7 +8,36 @@ const usersApi: Fastify.FastifyPluginCallback = function (
 ) {
   // Declare a route
   fastify.get("/api/users", function (request, reply) {
+    // TODO: validate request
     reply.send({ users: ["John", "Jack", "Jill"] });
+  });
+
+  fastify.get<{
+    Params: {
+      id: string;
+    };
+  }>("/api/users/:id", function (request, reply) {
+    const users = fastify.db.data.users || [];
+    const user = users.find((user) => user.id === request.params.id);
+    // TODO: handle not found
+    reply.send({ user });
+  });
+
+  fastify.post<{
+    Body: {
+      username: string;
+    };
+  }>("/api/butterflies", function (request, reply) {
+    // TODO: validate request body and return proper response
+    // TODO: avoid duplicate usernames
+    const newUser = {
+      id: nanoid(),
+      ...request.body,
+    };
+    fastify.db.data.users.push(newUser);
+
+    fastify.db.write();
+    reply.status(201).send({ butterfly: newUser });
   });
 
   done();
